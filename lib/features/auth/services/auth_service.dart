189a23d81795782cf3a33764d2097cc1c2cf09b7
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-09-09 17:14:17 +0530

    Signup backend and login complete

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:amazon_clone/constants/error_handling.dart';
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  // Sign up user function
+
+  void signUpUser(
+      {required BuildContext context,
+      required String email,
+      required String password,
+      required String name}) async {
+    try {
+      User user = User(
+          id: '',
+          name: name,
+          email: email,
+          password: password,
+          address: '',
+          type: '',
+          token: '');
+      http.Response response = await http.post(Uri.parse('$uri/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+
+      httpErrorHandle(
+          response: response,
+          context: context,
+          onSuccess: () {
+            ShowShankBar(
+              context,
+              'Account created successfully',
+            );
+          });
+    } catch (e) {
+      ShowShankBar(context, e.toString());
+    }
+  }
+}
