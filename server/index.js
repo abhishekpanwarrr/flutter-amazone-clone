87c5a97a0c71640999ac1ee2181b55add2a20963
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-09-09 16:24:27 +0530

    Added signup router in our backedn

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 // Package imports
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
 // File imports
-const authRouter = require("./routes/auth")
+const authRouter = require("./routes/auth");
 
 // INIt
 const PORT = 3000;
-const app = express()
-// 
-mongoose.connect().then(() => console.log("mongo db connected successfully")).catch(e => console.log("error in connecting mongodb", e))
+const MONGODB_URI =
+  "mongodb+srv://abhishekpanwarrr:P5rtS1DTK96gtujj@cluster0.s1tbtl9.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+//
+mongoose
+  .connect(MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Set a longer timeout here (in milliseconds)
+  })
+  .then(() => console.log("mongo db connected successfully"))
+  .catch((e) => console.log("error in connecting mongodb", e));
 
 // Middleware
-app.use(authRouter)
+app.use(express.json())
+app.use(authRouter);
 
-
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,28 @@
-const express = require("express")
+const express = require("express");
+const User = require("../model/user");
+const bcryptjs = require("bcryptjs");
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
-authRouter.post("/api/signup", (req,res) =>{
+authRouter.post("/api/signup", async (req, res) => {
+  try {
     // get data from client
-    const {name,email,password} = req.body;
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 10);
 
-})
-module.exports = authRouter 
+    let user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    user = await user.save();
+    return res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+module.exports = authRouter;
